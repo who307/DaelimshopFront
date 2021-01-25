ace706e1e8b432dfa12c15514fb9882523bfd407
moon gun <who307@naver.com>
2021-01-26 00:26:58 +0900

    Updates

---
+++ precache-manifest.e8a76b8f0d65011793fcfc1ca21f4ce5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd369af9b5e041efde501f84f18a4c92",
+    "url": "/shopFront/index.html"
+  },
+  {
+    "revision": "ae1b4652bb32dba07341",
+    "url": "/shopFront/static/css/main.04eae753.chunk.css"
+  },
+  {
+    "revision": "00e76662fa0e21b16b2d",
+    "url": "/shopFront/static/js/2.82af09d0.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/shopFront/static/js/2.82af09d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae1b4652bb32dba07341",
+    "url": "/shopFront/static/js/main.e49a5930.chunk.js"
+  },
+  {
+    "revision": "49589bda20946fb8dcfa",
+    "url": "/shopFront/static/js/runtime-main.45e18f63.js"
+  },
+  {
+    "revision": "673767b20b0c36bf9270cbd3ee379144",
+    "url": "/shopFront/static/media/clear-24px.673767b2.svg"
+  },
+  {
+    "revision": "d0831404339dbfbf5da926ec7b480452",
+    "url": "/shopFront/static/media/notes-24px.d0831404.svg"
+  },
+  {
+    "revision": "9acfe78b8e1cfbf4c1b1d1f31745b96b",
+    "url": "/shopFront/static/media/profile.9acfe78b.png"
+  },
+  {
+    "revision": "d378117f6d795255c01b62f5fcd40c6b",
+    "url": "/shopFront/static/media/search-24px.d378117f.svg"
+  }
+]);
